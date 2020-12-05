1bdb170a54f361ba1704071b2505416966cb80ba
Vivian Sunmola <viviansunmola@Vivians-Air.broadband>
2020-12-05 16:36:09 +0000

    Update xy coordinates

--- room_temp.py
+++ room_temp.py
@@ -4,12 +4,10 @@ import seaborn as sb
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-df = pd.read_csv("sensors.csv")
 # a = np.genfromtxt("sensors.csv", usecols=(2,3), skip_header=1, dtype=None, encoding=None)
 
+df=pd.read_csv("sensors.csv")
+coord=df[['x','y']]
+print(coord)
 
-x = df['x']
-y = df['y']
 
-print(x,y)
