efa33ea3dac5049c5459519fd13aee742af5041e
Vivian Sunmola <viviansunmola@gmail.com>
2020-12-12 23:56:46 +0000

    add colorbar for customized color thresholds

--- room_temp.py
+++ room_temp.py
@@ -13,7 +13,7 @@ plt.get_cmap('RdBu')
 plt.title('TEMPURATURE SERVICE - RTS')
 plt.xlabel('X_POSITION')
 plt.ylabel('Y_POSITION')
-plt.colorbar(label='Tempurature Values')
+
 
 blue_threshold = range (1,18)
 green_threshold = range(19,26)
@@ -21,6 +21,10 @@ amber_threshold = range(27,30)
 red_threshold = range(30, 100)
 yellow = None
 
+cmap = colormap
+cmap=cmap([blue_threshold, red_threshold])
+colormap(cmap)
+colorbar()
 
 plt.show()
 
