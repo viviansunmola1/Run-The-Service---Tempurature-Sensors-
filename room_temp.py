a1e94033898ec2505211e8cf89607209550d2720
Vivian Sunmola <viviansunmola@gmail.com>
2020-12-13 21:47:38 +0000

    add color bar

--- room_temp.py
+++ room_temp.py
@@ -5,26 +5,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# read file
 df=pd.read_csv("sensors.csv")
 
+# get x and y colomns for each sensor 
+# get temp reading for each sensor 
+
+
 plt.figure(figsize = (5,5)) # size of tab
+
 plt.hist2d(x=df['x'],y = df['y'], bins = 50,)
 plt.get_cmap('RdBu')
 plt.title('TEMPURATURE SERVICE - RTS')
 plt.xlabel('X_POSITION')
 plt.ylabel('Y_POSITION')
 
-
-blue_threshold = range (1,18)
-green_threshold = range(19,26)
-amber_threshold = range(27,30)
-red_threshold = range(30, 100)
-yellow = None
-
-cmap = colormap
-cmap=cmap([blue_threshold, red_threshold])
-colormap(cmap)
-colorbar()
-
+plt.colorbar()
 plt.show()
-
