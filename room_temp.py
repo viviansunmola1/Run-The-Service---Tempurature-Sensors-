72a526f86a7bec564a921d84a75d15a892f636c3
Vivian Sunmola <viviansunmola@gmail.com>
2021-02-18 01:38:15 +0000

    add class for all ER buttons

--- room_temp.py
+++ room_temp.py
@@ -5,26 +5,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import*
 
-#read from sensors file 
-er1 = pd.read_csv('ER1.csv')
-er2 = pd.read_csv('ER2.csv')
-er3 = pd.read_csv('ER3.csv')
+# #read from sensors file 
+# er1 = pd.read_csv('ER1.csv')
+# er2 = pd.read_csv('ER2.csv')
+# er3 = pd.read_csv('ER3.csv')
 
 tk = Tk()
 tk.title('Tempsensors')
-def ER_Options():
-    exit()
 
+class ER_Options():
+    def ER1():
+        df = pd.read_csv('ER1.csv')
+        pivot_table = df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        heatmap= sb.heatmap(pivot_table)
+        title = "RTS Tempurature Sensors ENTS Labs"
+        plt.title(title,fontsize=10)
+        
+    def ER2():
+        df = pd.read_csv('ER2.csv')
+        pivot_table = df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        heatmap= sb.heatmap(pivot_table)
+        title = "RTS Tempurature Sensors ENTS Labs"
+        plt.title(title,fontsize=10)
+        plt.show()
+        
+    def ER3():
+        df = pd.read_csv('ER3.csv')
+        pivot_table = df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        heatmap= sb.heatmap(pivot_table)
+        title = "RTS Tempurature Sensors ENTS Labs"
+        plt.title(title,fontsize=10)
+        plt.show()
+        
+        
 btn=Button(tk, text="ER1", 
-            command= ER_Options)
+            command= ER_Options.ER1)
 btn.pack()
 
 btn2= Button(tk, text="ER2",
-            command = ER_Options)
+            command = ER_Options.ER2)
 btn2.pack()
 
 btn3= Button(tk, text="ER3",
-            command = ER_Options)
+            command = ER_Options.ER3)
 btn3.pack()
 
 mainloop()
