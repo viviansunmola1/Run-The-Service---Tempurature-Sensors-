bd3183fdea07a6287807f453a19e7203bb7253a5
Vivian Sunmola <viviansunmola@gmail.com>
2021-02-23 18:37:16 +0000

    fix: centered and resized ER buttons, added visual heatmap to ER1

--- room_temp.py
+++ room_temp.py
@@ -5,56 +5,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import*
 
-# #read from sensors file 
-# er1 = pd.read_csv('ER1.csv')
-# er2 = pd.read_csv('ER2.csv')
-# er3 = pd.read_csv('ER3.csv')
-
 tk = Tk()
+tk.geometry('500x500')
 tk.title('Tempsensors')
 
 class ER_Options():
     def ER1():
-        plt.plot([1,2,3,4])
-        plt.ylabel('some numbers')
+        df = pd.read_csv('ER1.csv')
+        pivot_table= df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        heatmap= sb.heatmap(pivot_table)
+        title = "ER1 Server Room"
+        plt.title(title,fontsize=10)
         plt.show()
-
-        # df = pd.read_csv('ER1.csv')
-        # pivot_table = df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
-        # heatmap= sb.heatmap(pivot_table)
-        # title = "RTS Tempurature Sensors ENTS Labs"
-        # plt.title(title,fontsize=10)
-        # plt.show()
         
     def ER2():
         df = pd.read_csv('ER2.csv')
-        pivot_table = df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        pivot_table= df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
         heatmap= sb.heatmap(pivot_table)
-        title = "RTS Tempurature Sensors ENTS Labs"
+        title = "ER2 Server Room"
         plt.title(title,fontsize=10)
         plt.show()
         
     def ER3():
         df = pd.read_csv('ER3.csv')
-        pivot_table = df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        pivot_table= df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
         heatmap= sb.heatmap(pivot_table)
-        title = "RTS Tempurature Sensors ENTS Labs"
+        title = "ER3 Server Room"
         plt.title(title,fontsize=10)
         plt.show()
-        
-        
-btn=Button(tk, text="ER1", 
-            command= ER_Options.ER1)
-btn.pack()
 
-btn2= Button(tk, text="ER2",
-            command = ER_Options.ER2)
-btn2.pack()
+def configButtons(): 
+    btn=Button(tk, text="ER1", 
+                command= ER_Options.ER1,
+                height = 10, 
+                width = 10)
+    btn.pack()
 
-btn3= Button(tk, text="ER3",
-            command = ER_Options.ER3)
-btn3.pack()
+    btn2= Button(tk,
+                text="ER2",
+                command = ER_Options.ER2,
+                height = 10, 
+                width = 10)
+    btn2.pack()
 
+    btn3= Button(tk, 
+                text="ER3",
+                command = ER_Options.ER3,
+                height = 10, 
+                width = 10)
+
+    btn3.pack()
+
+    btn.config(anchor=CENTER)
+
+configButtons()
 mainloop()
 
 
