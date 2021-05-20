8b63f2b8ed8415b7ecc2b54d18d4eeb973a91abd
Vivian Sunmola <viviansunmola@gmail.com>
2021-05-20 10:09:34 +0100

    fix csv

--- room_temp.py
+++ room_temp.py
@@ -4,13 +4,15 @@ import seaborn as sb
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import tkinter as t
 from tkinter import*
 
 tk = Tk()
 tk.geometry('500x500')
-tk.title('ENTS Run The Service - Tempurature Sensors')
+tk.title('ENTS Run The Service - Temperature Sensors')
 
 newcolors = plt.get_cmap('viridis',100).colors
+
 
 newcolors[0: 18, :] = colors.to_rgba('blue')
 newcolors[18: 26, :] = colors.to_rgba('green')
@@ -19,20 +21,14 @@ newcolors[30:100, :] = colors.to_rgba('red')
 
 mycmap = colors.ListedColormap(newcolors)
 
+
+
 class ER_Options():
     def ER1():
         df = pd.read_csv('ER1.csv')
-
-         #FORMATING TEMPURATURE TO 1 DECIMAL PLACE 
-
-        # roundplaces = np.round(df['Tempurature'], 
-        #                decimals = 1) 
-        
-
-        
         pivot_table= df.pivot(index='Position_y',
                             columns= 'Position_x', 
-                            values = 'Tempurature')
+                            values = 'Temperature')
 
         heatmap= sb.heatmap(pivot_table,
                             annot=True,
@@ -41,26 +37,39 @@ class ER_Options():
                             cbar_kws= {'pad': .03, 'ticks': [0,18, 26, 27, 30, 100], })
     
 
-        heatmap.collections[0].colorbar.set_label("Tempurature Thresholds")
+        heatmap.collections[0].colorbar.set_label("Temperature Thresholds")
         plt.gca().invert_yaxis()
 
-    
         title = "ER1 Server Room"
         plt.title(title,fontsize=10)
         plt.show()
 
+       
+
         
     def ER2():
         df = pd.read_csv('ER2.csv')
-        pivot_table= df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
-        heatmap= sb.heatmap(pivot_table)
-        title = "ER2 Server Room"
+        pivot_table= df.pivot(index='Position_y',
+                            columns= 'Position_x', 
+                            values = 'Temperature')
+
+        heatmap= sb.heatmap(pivot_table,
+                            annot=True,
+                            cmap='rainbow',
+                            fmt = ".1f",
+                            cbar_kws= {'pad': .03, 'ticks': [0,18, 26, 27, 30, 100], })
+
+
+        heatmap.collections[0].colorbar.set_label("Temperature Thresholds")
+        plt.gca().invert_yaxis()
+
+        title = "ER1 Server Room"
         plt.title(title,fontsize=10)
         plt.show()
         
     def ER3():
         df = pd.read_csv('ER3.csv')
-        pivot_table= df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Tempurature')
+        pivot_table= df.pivot(index='Position_y' ,columns= 'Position_x', values = 'Temperature')
         heatmap= sb.heatmap(pivot_table)
         title = "ER3 Server Room"
         plt.title(title,fontsize=10)
